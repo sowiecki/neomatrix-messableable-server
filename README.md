57dc07c61fef1a985d7d52d526a88934cc5b8616
sowiecki <nase00@gmail.com>
2018-12-23 02:32:05 -0600

    Initial for reals this time.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Getting started
+
+Create `firmware/secrets.h` with your WiFi network credentials:
+
+```c
+const char *ssid = "ItHurtsWhenIP";
+const char *password = "hunter2";
+```
+
+# Libraries
+
+- [ESP8266/ESP32 WebServer library](https://github.com/bbx10/WebServer_tng/tree/8491a56cc4090f7f4f0edfc95c5bf9f6049d85cd)
+- [FastLED NeoMatrix](https://github.com/marcmerlin/FastLED_NeoMatrix)
+
+# Troubleshooting
+
+### _"Messageable" isn't a word!_
+
+It is now :)
---
+++ firmware/firmware.ino
@@ -0,0 +1,147 @@
+// Partially adapted from https://github.com/bbx10/WebServer_tng/blob/master/examples/AdvancedWebServer/AdvancedWebServer.ino
+
+#include <FastLED.h>
+#include <FastLED_NeoMatrix.h>
+
+#ifdef ESP8266
+#include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiClient.h>
+ESP8266WebServer server(80);
+#else
+#include <ESPmDNS.h>
+#include <WebServer.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+WebServer server(80);
+#endif
+
+#include "secrets.h"
+
+#define PIN 1
+
+// Neomatrix configuration
+#define MATRIX_TILE_WIDTH 7  // width of each individual matrix tile
+#define MATRIX_TILE_HEIGHT 7 // height of each individual matrix tile
+#define MATRIX_TILE_H 1      // number of matrices arranged horizontally
+#define MATRIX_TILE_V 1      // number of matrices arranged vertically
+#define mw (MATRIX_TILE_WIDTH * MATRIX_TILE_H)
+#define mh (MATRIX_TILE_HEIGHT * MATRIX_TILE_V)
+#define NUMMATRIX (mw * mh)
+
+uint8_t matrix_brightness = 32;
+CRGB matrixleds[NUMMATRIX];
+FastLED_NeoMatrix *matrix = new FastLED_NeoMatrix(
+    matrixleds, MATRIX_TILE_WIDTH,
+    MATRIX_TILE_HEIGHT,
+    MATRIX_TILE_H,
+    MATRIX_TILE_V,
+    NEO_MATRIX_TOP + NEO_MATRIX_LEFT + NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG + NEO_TILE_TOP + NEO_TILE_LEFT + NEO_TILE_PROGRESSIVE);
+
+const uint16_t colors[] = {
+  matrix->Color(255, 0, 0),
+  matrix->Color(0, 255, 0),
+  matrix->Color(0, 0, 255)
+};
+
+void setup() {
+  Serial.begin(115200);
+
+  FastLED.addLeds<NEOPIXEL, PIN>(matrixleds, NUMMATRIX);
+  matrix->begin();
+  matrix->setTextWrap(false);
+  matrix->setBrightness(40);
+  matrix->setTextColor(colors[0]);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+#ifdef ESP8266
+  if (MDNS.begin("esp8266")) {
+#else
+  if (MDNS.begin("esp32")) {
+#endif
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+  server.onNotFound(handleNotFound);
+
+  const char *headerkeys[] = { "text" };
+  size_t headerkeyssize = sizeof(headerkeys) / sizeof(char *);
+  server.collectHeaders(headerkeys, headerkeyssize);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+int x = mw;
+int pass = 0;
+String text;
+
+void loop() {
+  logDeviceData();
+
+  server.handleClient();
+  displayText(text);
+
+  delay(100);
+}
+
+void handleRoot() {
+  Serial.println("Enter handleRoot");
+  String header;
+  String content = "Successfully connected to device. ";
+  if (server.hasHeader("text")) {
+    content += "Scrolling text changed to: " + server.header("text");
+    text = server.header("text");
+  } else {
+    content += "No \"text\" header found, scrolling text has no been updated.";
+  }
+  server.send(200, "text/html", content);
+}
+
+void displayText(String text) {
+  matrix->fillScreen(0);
+  matrix->setCursor(x, 0);
+  matrix->print(text);
+  if (--x < -36) {
+    x = matrix->width();
+    if (++pass >= 3)
+      pass = 0;
+    matrix->setTextColor(colors[pass]);
+  }
+  matrix->show();
+}
+
+void logDeviceData() {
+  Serial.print("Device IP: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.print("Device MAC: ");
+  Serial.println(WiFi.macAddress());
+
+  Serial.print("Memory heap: ");
+  Serial.println(ESP.getFreeHeap());
+}
+
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+
+  server.send(404, "text/plain", message);
+}
