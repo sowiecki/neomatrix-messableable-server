791053c2163c7e1d5d3f33fc03c0c4595f547506
sowiecki <nase00@gmail.com>
2019-02-22 15:17:02 -0600

    Adjust settings to display fullsize fonts.

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -19,25 +19,31 @@ WebServer server(80);
 
 #include "secrets.h"
 
-#define PIN 1
+#define PIN 2
 
 // Neomatrix configuration
-#define MATRIX_TILE_WIDTH 7  // width of each individual matrix tile
-#define MATRIX_TILE_HEIGHT 7 // height of each individual matrix tile
+#define MATRIX_TILE_WIDTH 14  // width of each individual matrix tile
+#define MATRIX_TILE_HEIGHT 15 // height of each individual matrix tile
 #define MATRIX_TILE_H 1      // number of matrices arranged horizontally
 #define MATRIX_TILE_V 1      // number of matrices arranged vertically
 #define mw (MATRIX_TILE_WIDTH * MATRIX_TILE_H)
 #define mh (MATRIX_TILE_HEIGHT * MATRIX_TILE_V)
 #define NUMMATRIX (mw * mh)
 
-uint8_t matrix_brightness = 32;
+int x = mw;
+int pass = 0;
+char text[64] = "HACK THE OFFICE";
+int  pixelPerChar = 3;
+int  maxDisplacement;
+
+uint8_t matrix_brightness = 200;
 CRGB matrixleds[NUMMATRIX];
 FastLED_NeoMatrix *matrix = new FastLED_NeoMatrix(
     matrixleds, MATRIX_TILE_WIDTH,
     MATRIX_TILE_HEIGHT,
     MATRIX_TILE_H,
     MATRIX_TILE_V,
-    NEO_MATRIX_TOP + NEO_MATRIX_LEFT + NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG + NEO_TILE_TOP + NEO_TILE_LEFT + NEO_TILE_PROGRESSIVE);
+    NEO_MATRIX_BOTTOM + NEO_MATRIX_RIGHT + NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG + NEO_TILE_TOP + NEO_TILE_LEFT + NEO_TILE_ZIGZAG);
 
 const uint16_t colors[] = {
   matrix->Color(255, 0, 0),
@@ -51,8 +57,10 @@ void setup() {
   FastLED.addLeds<NEOPIXEL, PIN>(matrixleds, NUMMATRIX);
   matrix->begin();
   matrix->setTextWrap(false);
-  matrix->setBrightness(40);
+  matrix->setBrightness(matrix_brightness);
   matrix->setTextColor(colors[0]);
+
+  maxDisplacement = strlen(text) * pixelPerChar + matrix->width();
 
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -79,17 +87,14 @@ void setup() {
   Serial.println("HTTP server started");
 }
 
-int x = mw;
-int pass = 0;
-String text;
-
 void loop() {
   logDeviceData();
 
   server.handleClient();
+
   displayText(text);
 
-  delay(100);
+  delay(125);
 }
 
 void handleRoot() {
@@ -98,9 +103,10 @@ void handleRoot() {
   String content = "Successfully connected to device. ";
   if (server.hasHeader("text")) {
     content += "Scrolling text changed to: " + server.header("text");
-    text = server.header("text");
+
+    server.header("text").toCharArray(text, 64);
   } else {
-    content += "No \"text\" header found, scrolling text has no been updated.";
+    content += "No \"text\" header found, scrolling text has not been updated.";
   }
   server.send(200, "text/html", content);
 }
@@ -108,8 +114,11 @@ void handleRoot() {
 void displayText(String text) {
   matrix->fillScreen(0);
   matrix->setCursor(x, 0);
+  matrix->setTextSize(2.1);
   matrix->print(text);
-  if (--x < -36) {
+  
+  // https://community.particle.io/t/neomatrix-not-displaying-full-text-solved/9588/2
+  if (--x < -(maxDisplacement * 3)) {
     x = matrix->width();
     if (++pass >= 3)
       pass = 0;
